20c10f7b713bbfceabc46277665412fe5de57039
fcaitanob <60076276+fcaitanob@users.noreply.github.com>
2020-02-14 14:56:55 -0300

    nuevo contrato

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const contractSource = `
 `;
 
 //Address of the meme voting smart contract on the testnet of the aeternity blockchain
-const contractAddress = 'ct_2cZCZ5p3Tc9tx93LD5j6ePhUcq1jN9WoeWcuoiUBba2XkDZKAD';
+const contractAddress = 'ct_2G2SEP1JcSxzNujCNakJFxVwTgKRLxMFeuGsXZcnYNqPjipD9j';
 //Create variable for client so it can be used in different functions
 var client = null;
 //Create a new global array for the memes
